7b3902d9b7b2d902351f394ca23695f41b2aa82b
Satyam Anmol <221042144+anmolthecoder-hub@users.noreply.github.com>
2025-08-08 20:19:35 +0530

    Add files via upload

---
+++ if_conditions.py
@@ -0,0 +1,9 @@
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+if a<b:
+    print("b is greatest")
+elif b<a:
+     print("a is greater")
+
+else:
+    print("both are equal")
